e271bcb3c210fa0745d4521f83aac102076bc135
Tim977 <gentim977@gmail.com>
2023-07-07 21:38:40 -0400

    File rename main -> utils

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask, redirect, render_template, request
 
 from forms.user import InputTextForm
-from main import decode, encode
+from utils import decode, encode
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '9@K#3jP!2dR$5sF6gV%1hL&8kM4nT7bY*0cX2zQ1wE4'
