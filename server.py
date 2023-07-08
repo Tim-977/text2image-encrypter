f96238f802b9a36a6455004b4a9d8a46c3185908
Tim977 <gentim977@gmail.com>
2023-07-07 22:05:25 -0400

    Add BootstrapNavbar + NavigateButtons + Fix routes

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ app.config['SECRET_KEY'] = '9@K#3jP!2dR$5sF6gV%1hL&8kM4nT7bY*0cX2zQ1wE4'
 
 @app.route('/')
 def start():
-    return redirect('/encoder')
+    return render_template('welcome.html', title='welcome page')
 
 
 @app.route('/encoder', methods=['GET', 'POST'])
@@ -39,7 +39,7 @@ def encoder_success():
 @app.route('/decoder_success')
 def decoder_success():
     text = decode()
-    return render_template('encoder_success.html', text=text, title='Decoder success')
+    return render_template('decoder_success.html', text=text, title='Decoder success')
 
 
 if __name__ == '__main__':
